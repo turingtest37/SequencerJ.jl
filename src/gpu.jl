ab7839daa07c31c20bbf9760494a07ad3002defe
a5vzener <c.difficile@gmail.com>
2020-09-05 11:25:19 -0400

    CuArrays.allowscalar

--- src/gpu.jl
+++ src/gpu.jl
@@ -2,6 +2,6 @@ const USECUDA = CuArrays.functional()
 ENV["JULIA_CUDA_VERBOSE"] = USECUDA
 ENV["JULIA_CUDA_SILENT"] = !USECUDA
 
-allowscalar(false)
+CuArrays.allowscalar(false)
 
 wrap(A::AbstractArray) = (USECUDA && !(typeof(A) <: CuArray)) ? CuArray(A) : A
